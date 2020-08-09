c7d2850aa9d6afc48febf477209eb7939a0c4bf8
1810050 <67385245+1810050@users.noreply.github.com>
2020-08-09 09:52:34 +0600

    use of 'new' keyword

--- new.js
+++ new.js
@@ -12,14 +12,15 @@ const friendlyPerson = new Person("Hero", "Bak", 28000);
 console.log(friendlyPerson);
 
 // this keyword also works on function but this is old method
-function Person1(firstName, lastName, salary){
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.salary = salary;
+// function Person1(firstName, lastName, salary){
+//         this.firstName = firstName;
+//         this.lastName = lastName;
+//         this.salary = salary;
 
-}
-const oldPerson = new Person1("Grand", "Papa", 2000);
-console.log(oldPerson);
+// }
+// const oldPerson = new Person1("Grand", "Papa", 2000);
+// console.log(oldPerson);
+
 
 
 
