b584a82317345f45e8e968288a59b7f4e52ba434
1810050 <67385245+1810050@users.noreply.github.com>
2020-08-09 08:59:37 +0600

    scope, local, global, implicitly global

--- window.js
+++ window.js
@@ -3,11 +3,20 @@ function add(num1, num2){
     let result = num1+num2;
     console.log(`Result inside ${result}`);
     console.log(`Name inside ${name}`);
-    return result;
+    // return result;
+    function double(num){
+         
+        return num*2;
+
+    }
+    let total = double(result);
+    return total;
 }
 
 //console.log(`Result outside ${result}`);
-console.log(`Name outside ${name}`);
+// console.log(`Name outside ${name}`);
+// let sum = add(10, 20);
+// console.log(sum);
 let sum = add(10, 20);
 console.log(sum);
 
